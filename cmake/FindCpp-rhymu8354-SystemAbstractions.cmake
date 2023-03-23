b61248486df975fdac16b313de2c26db80f68a70
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-03-23 11:46:07 +0000

    Updated CMakeLists and moved to CPM

---
+++ cmake/FindCpp-rhymu8354-SystemAbstractions.cmake
@@ -0,0 +1,7 @@
+CPMAddPackage(
+    NAME    Cpp-rhymu8354-SystemAbstractions
+    GIT_REPOSITORY  https://github.com/tanja84dk/Cpp-rhymu8354-SystemAbstractions
+    GIT_BRANCH      Updated
+    GIT_TAG         939d1ee68f2306d9bc3ec8cf4c1619e3eb25e4d3
+
+)
